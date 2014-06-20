7553478418432079a5d2abe2ad3f3c7ab76fedee
Evan Vandegriff <evandegr@gmail.com>
2014-06-20 15:52:35 -0400

    replace inject tag with the source css

--- tasks/inject_css.js
+++ tasks/inject_css.js
@@ -14,37 +14,21 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('inject_css', 'Allows you to inject CSS into HTML files as part of the build process.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+	var src = grunt.file.expand(this.data.src);
+	var text = '';
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+	if (src) {
+		src.forEach(function (script) {
+	        text += grunt.file.read(script);
+	    });
+	} else {
+		grunt.log.error('Please specify a file to inject into the html.');
+		return;
+	}
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+	this.files.forEach(function (file) {
+		grunt.file.write(file.dest, grunt.file.read(file.src).replace('<!-- inject -->', '<style type="text/css">' + text + '</style>'));
+		grunt.log.ok('File injected'.blue + ' into ' + file.dest);
+	});
   });
-
 };
